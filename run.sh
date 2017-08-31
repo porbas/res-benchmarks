558790cab1f2ba4a8d0512ee2f708682f118486e
Michal Lomnicki <michal.lomnicki@gmail.com>
2017-08-31 19:29:06 +0200

    Use sane syntax

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ rm -f benchmarks/ips-state
 
 TARGETS=$(ls -1 apps)
 
-for target in ${TARGETS[@]}; do
+for target in $TARGETS; do
   echo "Benchmarking $target"
   pushd "apps/$target"
   rake db:drop db:create db:migrate
