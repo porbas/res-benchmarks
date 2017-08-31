ff477e81589668ed1785009a046ba63ca20131c4
Michal Lomnicki <michal.lomnicki@gmail.com>
2017-08-31 19:27:03 +0200

    Remove unused constant

--- generate.rb
+++ generate.rb
@@ -4,24 +4,14 @@ require "fileutils"
 require "yaml"
 
 Config = YAML.load_file("config.yml")
-
-TARGETS = {
-  "master"  => { github: "RailsEventStore/rails_event_store" },
-  "v0.15.0" => "= 0.15",
-  "v0.14.0" => "= 0.14",
-  "v0.13.0" => "= 0.13",
-  "v0.12.0" => "= 0.12",
-  "v0.11.0" => "= 0.11",
-  "v0.10.0" => "= 0.10",
-}
-
 RAILS_VERSION = Config.fetch("rails_version")
+VERSIONS = Config.fetch("versions")
 
 FileUtils.rm_rf("apps")
 FileUtils.mkdir_p("apps")
 Dir.chdir("apps")
 
-Config.fetch("versions").each do |name, gem_opts|
+VERSIONS.each do |name, gem_opts|
   system("rails _#{RAILS_VERSION}_ new #{name} --skip-spring")
   File.open("#{name}/Gemfile", "a") do |f|
     f.puts("gem 'rails_event_store', #{gem_opts.inspect}")
