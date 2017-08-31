1aab4c8d592cdd6f5f446b51d2f5f5552b0c1a37
Michal Lomnicki <michal.lomnicki@gmail.com>
2017-08-31 22:17:23 +0200

    More reliable method to use specified rails version for "rails new APP"
    Sometimes "rails _4.2.9_ new app_name" ignores the version tag and
    picks the newest rails. I didn't figure out why it happens.
    
    The new method makes sure that the correct version is picked
    1. Generate a minimal Gemfile with "gem 'rails', '4.2.9'"
    2. bundle install
    3. bundle exec rails new ...

--- generate.rb
+++ generate.rb
@@ -2,6 +2,7 @@
 
 require "fileutils"
 require "yaml"
+require "tmpdir"
 
 Config = YAML.load_file("config.yml")
 RAILS_VERSION = Config.fetch("rails_version")
@@ -11,14 +12,23 @@ FileUtils.rm_rf("apps")
 FileUtils.mkdir_p("apps")
 Dir.chdir("apps")
 
-VERSIONS.each do |name, gem_opts|
-  system("rails _#{RAILS_VERSION}_ new #{name} --skip-spring")
-  File.open("#{name}/Gemfile", "a") do |f|
-    f.puts("gem 'rails_event_store', #{gem_opts.inspect}")
-    f.puts("gem 'benchmark-ips'")
+Dir.mktmpdir do |dir|
+  gemfile = File.join(dir, "Gemfile")
+  File.open(gemfile, "w") do |f|
+    f.puts("source 'https://rubygems.org'")
+    f.puts("gem 'rails', '= #{RAILS_VERSION}'")
   end
-  Dir.chdir(name) do
-    system("bundle install")
-    system("rails g rails_event_store_active_record:migration")
+  system("bundle install --gemfile=#{gemfile}")
+
+  VERSIONS.each do |name, gem_opts|
+    system({"BUNDLE_GEMFILE" => gemfile}, "bundle exec rails new #{name} --skip-spring")
+    File.open("#{name}/Gemfile", "a") do |f|
+      f.puts("gem 'rails_event_store', #{gem_opts.inspect}")
+      f.puts("gem 'benchmark-ips'")
+    end
+    Dir.chdir(name) do
+      system("bundle install")
+      system("rails g rails_event_store_active_record:migration")
+    end
   end
 end
