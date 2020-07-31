d84f3e9e0506bb2606c8055a0a61f82ae060e6f4
Piotr Romańczuk <piotr.romanczuk@gmail.com>
2020-07-31 13:33:19 +0200

    change reading benchmark to read HUGE recordset in batches
    preparation: publish events in ONE transaction - it's much faster
    reading: read in batches, discard data after read (don't build huge array)

--- benchmarks/read_stream.rb
+++ benchmarks/read_stream.rb
@@ -2,7 +2,7 @@ require "benchmark/ips"
 require "securerandom"
 require "pathname"
 
-EVENTS_IN_STREAM = 50
+EVENTS_IN_STREAM = 200_000
 STREAM_NAME = "test-stream"
 
 es = RailsEventStore::Client.new
@@ -12,16 +12,22 @@ OrderCreated = Class.new(RailsEventStore::Event)
 SEARCH_PATH = Pathname.new(File.expand_path("../../apps/*", __FILE__))
 BASE_PATH   = Pathname.new(File.expand_path("../..", __FILE__))
 
-EVENTS_IN_STREAM.times do
-  es.publish(OrderCreated.new(data: { customer: "alice" }), stream_name: STREAM_NAME)
+customer = ("alice"*1024).freeze
+ActiveRecord::Base.transaction do
+  EVENTS_IN_STREAM.times do
+    es.publish(OrderCreated.new(data: { customer: customer }), stream_name: STREAM_NAME)
+  end
 end
 
 targets = Dir[SEARCH_PATH].map { |path| Pathname.new(path).relative_path_from(BASE_PATH).to_s.split("/")[1] }.sort
 
 Benchmark.ips do |x|
+  x.warmup = 0
   targets.each do |target|
     x.report(target) do
-      es.read.stream(STREAM_NAME).to_a
+      es.read.stream(STREAM_NAME).in_batches.each do |e|
+        e.data[:customer]
+      end
     end
 
     x.hold! File.expand_path("../ips-state", __FILE__)
